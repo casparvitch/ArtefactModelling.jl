d1c3e8598551af4f5b99e542b57c984a5ccf2dff
Documenter.jl <documenter@juliadocs.github.io>
2022-02-17 01:22:28 +0000

    build based on dd090eb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ArtefactModelling","category":"page"},{"location":"#ArtefactModelling","page":"Home","title":"ArtefactModelling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ArtefactModelling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ArtefactModelling]","category":"page"}]
+}
